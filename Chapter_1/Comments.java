1003819a2025552dd31ee1ac65ba3dd997aef139
EthanAstrologo-UCM <ethanastrologo@gmail.com>
2025-08-28 21:30:40 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,14 @@
+public class Comments {
+
+    public static void main(String[] args) {
+        
+        // single-line comment.
+        // used for brief, one-line explanations.
+        System.out.println("This program contains examples of Java comments.");
+
+        /*
+         * multi-line comment
+         * It is useful when you need to write a longer explanation
+         */
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Once in your life, you make a choice, ready to risk it all");
+        System.out.println("Deep in your soul, you hear a voice answering to the call");
+        System.out.println("Though you know that it won't be easy, it's a promise you made for love");
+        System.out.println("For the people that keep believing and the one that you're thinking of");
+    }
+}
