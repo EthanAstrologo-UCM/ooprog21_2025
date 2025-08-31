10446dc21806cc7611c577d8e9ffec2cc908624e
EthanAstrologo-UCM <ethanastrologo@gmail.com>
2025-08-31 17:54:20 +0800

    Add files via upload

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,17 @@
+public class FixDebugOne {
+   
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+List down the spotted bugs below:
+
+Bugs:
+1.  // class name "FixDebug One" contains a space, which is not allowed.
+2.  // main method is misspelled as "Main" instead of "main". Java is case-sensitive.
+3.  // "System" class is misspelled as "Systm".
+4.  // main method parameter "String args" should be "String[] args" to denote a string array.
+
+/*
