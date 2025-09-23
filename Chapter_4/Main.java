8b2eec8d165288c8a254544ee5431f360ee2df55
EthanAstrologo-UCM <ethanastrologo@gmail.com>
2025-09-23 19:26:35 +0800

    Delete Chapter_4/Main.java

--- Chapter_4/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        DebugCircle myCircle = new DebugCircle(5.0);
-
-        System.out.println("Radius: " + myCircle.getRadius());
-        System.out.println("Diameter: " + myCircle.getDiameter());
-        System.out.println("Area: " + myCircle.getArea());
-    }
-}
